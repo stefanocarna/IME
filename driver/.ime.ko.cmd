a07a4319c776e6b7c1e65b7e67ef19748fd8e528
serenaferracci <serenaferracci95@gmail.com>
2019-03-11 16:42:15 +0100

    enabled PEBS

--- driver/.ime.ko.cmd
+++
@@ -1,1 +0,0 @@
-cmd_/home/serenaferracci/Desktop/IME/driver/ime.ko := ld -r -m elf_x86_64 -z max-page-size=0x200000 -T ./scripts/module-common.lds --build-id  -o /home/serenaferracci/Desktop/IME/driver/ime.ko /home/serenaferracci/Desktop/IME/driver/ime.o /home/serenaferracci/Desktop/IME/driver/ime.mod.o ;  true
